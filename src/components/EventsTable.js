25560d400e16c12e35f0a05dcbe1f6c8855eb30c
Ed Safo <esafo800@gmail.com>
2021-04-30 14:50:12 -0400

    fetch session events from xhr/server request

--- src/components/EventsTable.js
+++ src/components/EventsTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -14,52 +14,77 @@ const useStyles = makeStyles({
   },
 });
 
-const sessionEvents = [
-    {
-        "event_id": "device1.0",
-        "device_id": "device1",
-        "button_id": "1",
-        "start_time": "2021-04-29T12:30:00.1679734Z",
-        "end_time": "2021-04-29T12:30:01.1679734Z"
-    },
-    {
-        "event_id": "device1.1",
-        "device_id": "device1",
-        "button_id": "1",
-        "start_time": "2021-04-29T12:40:00.1679734Z",
-        "end_time": "2021-04-29T12:40:01.1679734Z"
+
+class EventsTable extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://autismhackathon2021apinet3.azurewebsites.net/api/GetButtonEventsForSession?code=UzXXjTrKL6V8HRwVpKhVdTu3Wf22uP3W9/PxPJR/u/GmZSYqAGoMog==&session_id=session1")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <TableContainer component={Paper}>
+          <Table className="table" aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Event Id</TableCell>
+                <TableCell>Device Id</TableCell>
+                <TableCell>Button Id</TableCell>
+                <TableCell>Start Time</TableCell>
+                <TableCell>End Time</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {items.map((sessionEvent) => (
+                <TableRow key={sessionEvent.event_id}>
+                  <TableCell component="th" scope="row">
+                    {sessionEvent.event_id}
+                  </TableCell>
+                  <TableCell align="right">{sessionEvent.device_id}</TableCell>
+                  <TableCell align="right">{sessionEvent.button_id}</TableCell>
+                  <TableCell align="right">{sessionEvent.start_time}</TableCell>
+                  <TableCell align="right">{sessionEvent.end_time}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      );
     }
-];
-
-export default function EventsTable() {
-  const classes = useStyles();
-
-  return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Event Id</TableCell>
-            <TableCell>Device Id</TableCell>
-            <TableCell>Button Id</TableCell>
-            <TableCell>Start Time</TableCell>
-            <TableCell>End Time</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {sessionEvents.map((sessionEvent) => (
-            <TableRow key={sessionEvent.event_id}>
-              <TableCell component="th" scope="row">
-                {sessionEvent.event_id}
-              </TableCell>
-              <TableCell align="right">{sessionEvent.device_id}</TableCell>
-              <TableCell align="right">{sessionEvent.button_id}</TableCell>
-              <TableCell align="right">{sessionEvent.start_time}</TableCell>
-              <TableCell align="right">{sessionEvent.end_time}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+  }
 }
+
+export default EventsTable;
