e0e512a194136646f502809f24cc8388aa05b24a
mickelsonmichael <mickelsonmichael@gmail.com>
2022-03-31 11:33:53 -0600

    Create basic bootstrap nav

--- web/src/components/Nav/Nav.tsx
+++ web/src/components/Nav/Nav.tsx
@@ -1,11 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Navbar, NavbarToggler, Collapse, Nav as Navigation, NavItem, NavLink as BsNavLink } from "reactstrap";
 
-const Nav = () => (
-    <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/calendar">Calendar</Link></li>
-    </ul>
-);
+const Nav = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(b => !b);
+
+    return (
+        <Navbar color="light" light expand="md">
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+                <Navigation className="ml-auto" navbar>
+                    <NavItem>
+                        <BsNavLink tag={NavLink} to="/">Home</BsNavLink>
+                    </NavItem>
+                    <NavItem>
+                        <BsNavLink tag={NavLink} to="/calendar">Calendar</BsNavLink>
+                    </NavItem>
+                </Navigation>
+            </Collapse>
+        </Navbar>
+    );
+}
 
 export default Nav;
