8199ad422ccdea78d9f74f74e1dbc60efd8e74b9
mickelsonmichael <mickelsonmichael@gmail.com>
2022-03-31 13:46:03 -0600

    More patient info

--- web/src/components/PatientInfo/PatientInfo.tsx
+++ web/src/components/PatientInfo/PatientInfo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import usePatient from "../../hooks/usePatient";
 
 const PatientInfo = () => {
@@ -7,7 +8,19 @@ const PatientInfo = () => {
     const { patient } = usePatient(Number(patientId));
 
     return (
-        <div>Patient Info For: {patient?.firstName} {patient?.surname}</div>
+        <div className="mx-1">
+            <div>
+                <Link to="/">Patients</Link>
+                <span className="mx-1">&gt;</span>
+                <span className="text-muted text-capitalize">
+                    {patient?.firstName}
+                    &nbsp;
+                    {patient?.surname}
+                </span>
+            </div>
+            <br />
+            <h2 className="text-capitalize">{patient?.firstName} {patient?.surname}</h2>
+        </div>
     )
 }
 
--- web/src/hooks/usePatients.ts
+++ web/src/hooks/usePatients.ts
@@ -1,16 +1,48 @@
 import { useQuery } from "react-query";
-import Patient from "../models/Patient";
+import PatientDetails from "../models/PatientDetails";
 
 const fetchPatients = async () => {
     return [
-        { patientId: 1, firstName: "mike", surname: "mickelson" },
-        { patientId: 2, firstName: "ken", surname: "lastname" },
-        { patientId: 3, firstName: "graham", surname: "alexander-thompson" }
-    ];
+        {
+            patientId: 1,
+            firstName: "mike",
+            surname: "mickelson",
+            behaviorsList: [
+                {
+                    behaviorId: 1,
+                    description: "Behavior 1",
+                    type: "Event",
+                },
+                {
+                    behaviorId: 2,
+                    description: "Behavior 2",
+                    type: "Duration",
+                }
+            ]
+        },
+        {
+            patientId: 2,
+            firstName: "ken",
+            surname: "lastname",
+            behaviorsList: [
+                {
+                    behaviorId: 3,
+                    description: "Behavior 3",
+                    type: "TimeToResponse",
+                }
+            ]
+        },
+        {
+            patientId: 3,
+            firstName: "graham",
+            surname: "alexander-thompson",
+            behaviorsList: []
+        }
+    ] as PatientDetails[];
 }
 
 const usePatients = () => {
-    const { data, isError, isLoading } = useQuery<Patient[]>(["/patients"], fetchPatients);
+    const { data, isError, isLoading } = useQuery<PatientDetails[]>(["/patients"], fetchPatients);
 
     return {
         patients: data,
