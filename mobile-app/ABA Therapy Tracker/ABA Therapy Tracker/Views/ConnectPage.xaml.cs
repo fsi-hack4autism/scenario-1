0b05b6b4d3c2e63fca89d707ea3b1e45619b0df1
abhimonga <abhimonga16@gmail.com>
2022-04-01 16:17:35 +0530

    Added bluetooth connectivity and IOT replica page

--- mobile-app/ABA Therapy Tracker/ABA Therapy Tracker/Views/ConnectPage.xaml.cs
+++ mobile-app/ABA Therapy Tracker/ABA Therapy Tracker/Views/ConnectPage.xaml.cs
@@ -12,20 +12,37 @@ namespace ABA_Therapy_Tracker.Views
         public ConnectPage()
         {
             InitializeComponent();
-            CrossBleAdapter.Current.Scan(new ScanConfig() { ServiceUuids = new List<Guid>() { new Guid("00afbae4-0000-4233-bb16-1e3500152342") } }).Subscribe(scanResult => {
-                scanResult.Device.Connect();
+        }
 
-                scanResult.Device.WhenAnyCharacteristicDiscovered().Subscribe(characteristic => {
+        private void connectBLE(object sender, EventArgs e)
+        {
+        
+            ScanConfig scanConfig = new ScanConfig() { ServiceUuids = new List<Guid>() { new Guid("00afbae4-0000-4233-bb16-1e3500152342"),new Guid("a2075c7d-39b2-4750-aede-4bfc485547a2") } };
+
+             CrossBleAdapter.Current.ScanForUniqueDevices().Subscribe(scanResult => {
+                 scanResult.Connect();
+
+                scanResult.WhenAnyCharacteristicDiscovered().Subscribe(characteristic => {
                     // read, write, or subscribe to notifications here
                     var result = characteristic.Read(); // use result.Data to see response
-                    //characteristic.Write(bytes);
+                    result.Subscribe(res =>
+                    {
+                      var DeviceMac = res.Data;
+                       var DeviceUUID = res.Characteristic;
+                        Console.WriteLine("Result------------", res);
 
+                    });
+                    
+                    //characteristic.Write(bytes);
+                    Console.WriteLine(result);
                     characteristic.EnableNotifications();
                     characteristic.WhenNotificationReceived().Subscribe(notification => {
                         //result.Data to get at response
                     });
                 });
+                 CrossBleAdapter.Current.StopScan();
             });
+
         }
     }
 }
